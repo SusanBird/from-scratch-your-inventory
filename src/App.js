0466d0dbe73be6b281f8ff9ac1adf794e84de6a1
SusanBird <susanbird391@gmail.com>
2022-05-12 10:22:51 -0700

    created links and logout function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
+import { logout } from './services/fetch-utils';
+
+async function handleLogout() {
+  await logout();
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/create">Create</Link>
+          </li>
+          <li>
+            <Link to="/restaurants">View Restaurants</Link>
+          </li>
+          <li>
+            <button onClick={handleLogout}>Log Out</button>
+          </li>
+        </ul>
+      </nav>
+
     </div>
   );
 }
---
+++ src/services/fetch-utils.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { client } from './client';
+
+export async function logout() {
+  await client.auth.signOut();
+}
