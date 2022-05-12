4a79440f5b76db835595e5b0925c50cf76c62ceb
SusanBird <susanbird391@gmail.com>
2022-05-12 10:45:41 -0700

    built structure of Auth page

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -2,6 +2,31 @@ import React from 'react';
 
 export default function AuthPage() {
   return (
-    <div>AuthPage</div>
+    <><div>
+      <form>
+        <h3>Sign Up</h3>
+        <label>
+            email
+          <input />
+        </label>
+        <label>
+            password
+          <input />
+        </label>
+      </form>
+    </div>
+    <div>
+      <form>
+        <h3>Sign In</h3>
+        <label>
+            email
+          <input />
+        </label>
+        <label>
+            password
+          <input />
+        </label>
+      </form>
+    </div></>
   ); 
 }
